1e07672aa19f6a083aaf21e9547a3a23a216fd02
gabrielmendezsoares <gabrielmendezsoares@gmail.com>
2023-06-25 23:57:48 -0300

    [Chore]: Requisito 9

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,6 @@
 export const SET_EMAIL = 'SET_EMAIL';
 export const SET_NAME = 'SET_NAME';
 export const SET_SCORE = 'SET_SCORE';
-export const SET_TIMER = 'SET_TIMER';
 
 export const setEmailAction = (gravatarEmail) => ({
   type: SET_EMAIL,
@@ -17,9 +16,4 @@ export const setNameAction = (name) => ({
 export const setScoreAction = (score) => ({
   type: SET_SCORE,
   payload: score,
-});
-
-export const setTimerAction = (status) => ({
-  type: SET_TIMER,
-  payload: status,
 });
--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -2,42 +2,36 @@ import {
   SET_EMAIL,
   SET_NAME,
   SET_SCORE,
-  SET_TIMER,
 } from '../actions/index';
 
-// muda o nome das propriedades do estado global player, adequando conforme especificações do readme
+// muda o nome das propriedades do estado global player,
+// adequando conforme especificações do readme
+
 const INITIAL_STATE = {
   name: '',
-  assertions: 0,
-  score: 0,
   gravatarEmail: '',
-  timer: false,
+  score: 0,
+  assertions: 0,
 };
 
 const player = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case SET_EMAIL:
-    return {
-      ...state,
-      gravatarEmail: action.payload,
-    };
-
   case SET_NAME:
     return {
       ...state,
       name: action.payload,
     };
 
+  case SET_EMAIL:
+    return {
+      ...state,
+      gravatarEmail: action.payload,
+    };
+
   case SET_SCORE:
     return {
       ...state,
       score: action.payload,
-    };
-
-  case SET_TIMER:
-    return {
-      ...state,
-      timer: action.payload,
     };
 
   default:
